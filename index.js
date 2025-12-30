a60ecc4d5c6ff67d86c49fe831c2db0a8c1ded1e
Arpan <arpandey.web@gmail.com>
2025-12-30 23:04:01 +0600

    database design and structure

---
+++ index.js
@@ -0,0 +1,29 @@
+/*
+Part 2: SQL Queries
+Write SQL queries based on your designed schema.
+
+Check Sample Input/Output: To understand the expected results for each query, please refer to the Sample Query Results (QUERY.md) file.
+
+Query 1: JOIN
+Retrieve booking information along with:
+
+Customer name
+Vehicle name
+Concepts used: INNER JOIN
+
+Query 2: EXISTS
+Find all vehicles that have never been booked.
+
+Concepts used: NOT EXISTS
+
+Query 3: WHERE
+Retrieve all available vehicles of a specific type (e.g. cars).
+
+Concepts used: SELECT, WHERE
+
+Query 4: GROUP BY and HAVING
+Find the total number of bookings for each vehicle and display only those vehicles that have more than 2 bookings.
+
+Concepts used: GROUP BY, HAVING, COUNT  
+
+/*
